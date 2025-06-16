cb39fd7227d678dc9371b262aab7e7d47c96c926
Rouven <rringger@bzz.ch>
2025-06-16 09:54:04 +0200

    merge frontend/backend code

---
+++ frontend/src/app/export/export.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-export',
+  templateUrl: './export.component.html',
+  styleUrls: ['./export.component.css']
+})
+export class ExportComponent {
+  lastExport: Date | null = null;
+
+  exportCSV(): void {
+    console.log('Exportiere CSV...');
+    // Hier würdest du die exportierte Datei generieren und runterladen lassen
+    this.lastExport = new Date();
+  }
+
+  exportJSON(): void {
+    console.log('Exportiere JSON...');
+    // Hier würdest du JSON-Dateien aus vorhandenen Daten erzeugen
+    this.lastExport = new Date();
+  }
+}
